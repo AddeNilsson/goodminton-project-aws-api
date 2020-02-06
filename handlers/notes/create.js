ed5d71680a2bf49aebbce8cbc7641811695b3f3d
Adde Nilsson <adde.a.nilsson@gmail.com>
2020-02-06 09:05:39 +0100

    restructure notes

--- handlers/notes/create.js
+++ handlers/notes/create.js
@@ -1,7 +1,7 @@
 import uuid from 'uuid';
 
-import dbCall from './libs/dynamodb-lib';
-import { success, failure } from './libs/response-lib';
+import dbCall from '../../libs/dynamodb-lib';
+import { success, failure } from '../../libs/response-lib';
 
 export async function main(event, context, callback) {
   const data = JSON.parse(event.body); // i.e. req.body
--- handlers/notes/get.js
+++ handlers/notes/get.js
@@ -1,5 +1,5 @@
-import dbCall from './libs/dynamodb-lib';
-import { success, failure } from './libs/response-lib';
+import dbCall from '../../libs/dynamodb-lib';
+import { success, failure } from '../../libs/response-lib';
 
 export async function main(event, context) {
   const params = {
